d1328cb0bd75bb2d8d7159eaefbf3428ab7354e5
Apoorv <apoorvsharma2603@gmail.com>
2024-09-25 23:26:10 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Background from './assets/Background'
 import Navbar from './assets/Navbar'
-import Home from './assets/Home'
-import AboutMe from './assets/AboutMe'
 
 const App = () => {
   return (
