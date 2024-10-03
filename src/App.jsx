8492811db8f0489672dff74745fcee58d95f6137
Apoorv <apoorvsharma2603@gmail.com>
2024-10-03 18:47:32 +0530

    updated home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Background from './assets/Background'
 import Navbar from './assets/Navbar'
 import Home from './assets/Home'
-import AboutMe from './assets/AboutMe'
-import Experiences from './assets/Experiences'
 
 const App = () => {
   return (
@@ -11,8 +9,6 @@ const App = () => {
       <Background/>
       <Navbar />
       <Home />
-      <AboutMe />
-      <Experiences />
     </div>
   )
 }
